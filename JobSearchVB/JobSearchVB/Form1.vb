68ce251e31eb21b53663ce7f6047e49dafdb7138
wink <kwingertfkt@gmail.com>
2018-10-10 12:57:00 -0400

    V 1.00

---
+++ JobSearchVB/JobSearchVB/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim Folder_Path As String
+        If TextBox1.TextLength = 4 Then
+            If TextBox1.Text.Substring(0, 1) = "6" Then
+                Folder_Path = "T:\6000s\"
+                Dim files As String() = IO.Directory.GetDirectories(Folder_Path)
+                For Each file As String In files
+                    Dim fileName As String = IO.Path.GetFileName(file)
+                    If fileName.Contains(TextBox1.Text) Then
+                        Process.Start(Folder_Path & fileName)
+                        Close()
+                        End
+                    End If
+                Next
+            End If
+            If TextBox1.Text.Substring(0, 1) = "7" Then
+                Folder_Path = "T:\7000s\"
+                Dim files As String() = IO.Directory.GetDirectories(Folder_Path)
+                For Each file As String In files
+                    Dim fileName As String = IO.Path.GetFileName(file)
+                    If fileName.Contains(TextBox1.Text) Then
+                        Process.Start(Folder_Path & fileName)
+                        Close()
+                        End
+                    End If
+                Next
+            End If
+            If TextBox1.Text.Substring(0, 1) = "8" Then
+                Folder_Path = "T:\8000s\"
+                Dim files As String() = IO.Directory.GetDirectories(Folder_Path)
+                For Each file As String In files
+                    Dim fileName As String = IO.Path.GetFileName(file)
+                    If fileName.Contains(TextBox1.Text) Then
+                        Process.Start(Folder_Path & fileName)
+                        Close()
+                        End
+                    End If
+                Next
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ JobSearchVB/JobSearchVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("JobSearchVB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("JobSearchVB")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e459f5b8-7dcc-4e07-b636-03a46fb3ece6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
